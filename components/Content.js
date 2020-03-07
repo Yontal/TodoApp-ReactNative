0be8299b768f0233fd691c8a3a0192440f28584c
Yontal <eq2db@yandex.ru>
2020-03-07 15:24:28 +0300

    add swipe to item

--- components/Content.js
+++ components/Content.js
@@ -1,19 +1,78 @@
-import React from 'react'
-import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native'
+import React, { useState } from 'react'
+import {View, Text, StyleSheet, TouchableOpacity, TouchableHighlight} from 'react-native'
 import {Item} from './Item'
+import { SwipeListView } from 'react-native-swipe-list-view';
+
 
 export const Content = ({items, onRemove}) => {
 
+    const [listData, setListData] = useState(items)
+
+    const closeRow = (rowMap, rowKey) => {
+        if (rowMap[rowKey]) {
+            rowMap[rowKey].closeRow();
+        }
+    };
+
+    const deleteRow = (rowMap, rowKey) => {
+        closeRow(rowMap, rowKey);
+        const newData = [...listData];
+        const prevIndex = listData.findIndex(item => item.key === rowKey);
+        newData.splice(prevIndex, 1);
+        setListData(newData);
+    };
+
+    const onRowDidOpen = rowKey => {
+        console.log('This row opened', rowKey);
+    };
+
+    const renderItem = data => (
+        <TouchableHighlight
+            onPress={() => console.log('You touched me')}
+            style={styles.rowFront}
+            underlayColor={'#AAA'}
+        >
+            <View>
+                <Text>{data.item.itemValue}</Text>
+            </View>
+        </TouchableHighlight>
+    );
+
+    const renderHiddenItem = (data, rowMap) => (
+        <View style={styles.rowBack}>
+            <Text></Text>
+            <TouchableOpacity
+                style={[styles.backRightBtn, styles.backRightBtnLeft]}
+                onPress={() => closeRow(rowMap, data.item.key)}
+            >
+                <Text style={styles.backTextWhite}>Close</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={[styles.backRightBtn, styles.backRightBtnRight]}
+                onPress={() => {
+                    deleteRow(rowMap, data.item.key)
+                    onRemove(data.item.key)}
+                            }
+            >
+                <Text style={styles.backTextWhite}>Delete</Text>
+            </TouchableOpacity>
+        </View>
+    );
 
     return(
     <View style={styles.content}>
-        <FlatList
-            keyExtractor={item => item.key}
-            data={items}
-            renderItem={({item}) => (
-                <TouchableOpacity onLongPress={() => onRemove(item.key)}><Item item={item.itemValue} /></TouchableOpacity>
-                )}
-        />
+         <SwipeListView
+                data={items}
+                renderItem={renderItem}
+                renderHiddenItem={renderHiddenItem}
+                leftOpenValue={75}
+                rightOpenValue={-150}
+                previewRowKey={'0'}
+                previewOpenValue={-40}
+                previewOpenDelay={3000}
+                onRowDidOpen={onRowDidOpen}
+                disableRightSwipe={true}
+            />
     </View>
     );
 }
@@ -23,5 +82,44 @@ const styles = StyleSheet.create({
         width: '100%',
         backgroundColor: '#EAEAEA',
         flex: 15
+    },
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+    },
+    backTextWhite: {
+        color: '#FFF',
+    },
+    rowFront: {
+        alignItems: 'center',
+        backgroundColor: '#CCC',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        justifyContent: 'center',
+        height: 50,
+    },
+    rowBack: {
+        alignItems: 'center',
+        backgroundColor: '#DDD',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+    },
+    backRightBtn: {
+        alignItems: 'center',
+        bottom: 0,
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        width: 75,
+    },
+    backRightBtnLeft: {
+        backgroundColor: 'blue',
+        right: 75,
+    },
+    backRightBtnRight: {
+        backgroundColor: 'red',
+        right: 0,
     }
 })
